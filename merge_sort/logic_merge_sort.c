0b2ca207a6832fa1b91956fb059cfeeaa12c182f
bhimanikritik <bhimanikritik@gmail.com>
2015-05-25 23:05:32 +0530

    resolved some bugs in merge_sort/logic_merge_sort.c

--- merge_sort/logic_merge_sort.c
+++ merge_sort/logic_merge_sort.c
@@ -1,6 +1,6 @@
-void merge(int *,int *,int *);
-void merge_sort(int *, int );
-void divide_in_two(int *,int *,int *);
+void merge(int *,int ,int *,int *);
+void merge_sort(int *,int );
+void divide_in_two(int *,int ,int *,int *);
 void output_sort(int *, int );
 void merge_sort(int *list, int n)
 {
@@ -8,12 +8,12 @@ void merge_sort(int *list, int n)
 	{	return ;}
 	else
 	{
+		output_sort(list,n);
 		int first_half[(n / 2)],second_half[(n % 2 != 0 ? (n / 2) + 1 : (n / 2))];
-		printf("%d %d\n",(n / 2),(n % 2 != 0 ? (n / 2) + 1 : (n / 2)) );
-		divide_in_two(list,first_half,second_half);
-		merge_sort(first_half,sizeof(first_half) / sizeof(first_half[0]));
-		merge_sort(second_half,sizeof(second_half) / sizeof(second_half[0]));
-		merge(list,first_half,second_half);
+		divide_in_two(list,n,first_half,second_half);
+		merge_sort(first_half,(n / 2));
+		merge_sort(second_half,(n % 2 != 0 ? (n / 2) + 1 : (n / 2)));
+		merge(list,n,first_half,second_half);
 	}
 }
 /*
@@ -21,26 +21,24 @@ void merge_sort(int *list, int n)
 *is passed by refrence the two arrays that were created in the function merge_sort
 *
 */
-void divide_in_two(int *list,int *first_half,int *second_half)
+void divide_in_two(int *list,int list_size,int *first_half,int *second_half)
 {
-	int i,n = sizeof(list) / sizeof(list[0]) ;
-	for(i = 0; i < n; i ++)
+	int i ;
+	for(i = 0; i < list_size; i ++)
 	{
-		if (i < (n / 2))
+		if (i < (list_size / 2))
 		{
 			first_half[i] = list[i];
 		}
 		else
 		{
-			second_half[i - (n / 2)] = list[i];
+			second_half[i - (list_size / 2)] = list[i];
 		}
 	}
-	output_sort(first_half,(n / 2));
-	output_sort(second_half,sizeof(second_half) / sizeof(second_half[0]));
 }
-void merge(int *list,int *first_half,int *second_half)
+void merge(int *list,int list_size,int *first_half,int *second_half)
 {
-	int k = 0,a = sizeof(first_half) / sizeof(first_half[0]) , b = sizeof(second_half) / sizeof(second_half[0]) , i = 0 , j = 0 ;
+	int k = 0,a = list_size / 2 , b = (list_size % 2 != 0 ? (list_size / 2) + 1 : (list_size / 2)) , i = 0 , j = 0 ;
 	while(i < a && j < b)
 	{
 		if (first_half[i] < second_half[j])
@@ -62,7 +60,5 @@ void merge(int *list,int *first_half,int *second_half)
 	{
 		list[k] = first_half[i];
 	}
-	else
-	{}
 	output_sort(list,a + b);
 }
