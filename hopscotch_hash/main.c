938fc764d8b466d1b14b813fccc1b987ee90f859
codelec <bhimanikritik@gmail.com>
2015-07-10 10:22:09 +0530

    regular update

--- hopscotch_hash/main.c
+++ hopscotch_hash/main.c
@@ -1,12 +1,14 @@
+/*
+*credits to the following paper for providing a good insight into hopscotch hashing
+people.csail.mit.edu/shanir/publications/disc2008_submission_98.pdf
+*/
 #define HOP_SIZE
 #define ADDR_RANGE
 #define MAX_SEGMENTS
 #include <stdint.h>
 #include <stdbool.h>
-typedef struct
-{
-	uint32_t key;
-}KEY;
+/*seperate struct for data to allow future feature addons in
+the form of DATA*/
 typedef struct
 {
 	uint32_t data;
@@ -14,59 +16,95 @@ typedef struct
 typedef struct 
 {
 	uint32_t _hop_info ;
-	KEY	*key;
-	DATA *data; 
+	uint32_t key;
+	DATA data; 
 }BUCKET;
-BUCKET segments_arr[][ADDR_RANGE];
-bool contains(KEY *check_key)
+/*all the buckets in this array are organized in the form of
+*2D array so if in case there is need for extra BUCKETS then
+*the number of segments can be doubled or simply incremented
+*/
+BUCKET segments_arr[][ADDR_RANGE];//declared global at present will be taken care of later on , at present testing
+/*current_max_segment is to make sure that
+*segment should not be incremented
+*/
+uint32_t current_max_segment;//start from zero
+bool contains(uint32_t *check_key)
 {
-	uint32_t hash = hash_calc(check_key->key);
+	uint32_t hash = hash_calc(*check_key);
 	uint32_t segment = hash & segment_mask ;
-	uint32_t bucket_id = hash & bucket_mask ;
-	BUCKET *start_bucket = segments_arr[segment][bucket_id];
-	uint32_t start_hop_info = start_bucket->_hop_info;
+	uint32_t start_bucket_id = hash & bucket_mask ;
+	uint32_t start_hop_info = segments_arr[segment][start_bucket_id]->_hop_info;
+	uint32_t synergy = ADDR_RANGE - start_bucket_id - 1;
+	bool flag1 = 0,flag2 = 0;
+	if (current_max_segment - 1 == segment)
+		flag1 = 1;
 	uint32_t mask = 1, count = 0;
 	for (;count =< (HOP_SIZE - 1); count++ , mask <<= 1)
 	{
-		if ((mask & start_hop_info) && (segments_arr[segment][bucket_id + count]->key->key == check_key->key))
-		{
+		if ((mask & start_hop_info) && (segments_arr[segment][start_bucket_id + count].key == (*check_key)))
 			return true;
+		if (flag1 && ((count + start_bucket_id + 1) == ADDR_RANGE))
+			break;//since the next row is not yet allocated neither initialized
+		if (!flag1 && !flag2 && count => synergy)
+		{
+			flag2 = 1;
+			segment++;
 		}
 	}
 	return false;
 }
-bool add(KEY *key,DATA *data)
+void find_near_free_distance(uint32_t *free_distance,BUCKET **free_bucket)
+{
+
+}
+bool add(uint32_t *key,DATA *data)
 {
 	/*if (contains(key))
-	{
 		return true;
-	}*/
-	uint32_t hash = hash_calc(check_key->key);
+	*/
+	uint32_t hash = hash_calc(*key);
 	uint32_t segment = hash & segment_mask ;
-	uint32_t bucket_id = hash & bucket_mask ;
-	BUCKET *start_bucket = segments_arr[segment][bucket_id];
+	uint32_t start_bucket_id = hash & bucket_mask ;
+	BUCKET *start_bucket = &segments_arr[segment][start_bucket_id];
+	BUCKET *free_bucket;
 	uint32_t start_hop_info = start_bucket->_hop_info;
-	uint32_t free_distance = 0;
 	/*when free_distance crosses synergy it means
 	that it has to now move to the next row in the
 	segments_arr since it has crossed the ADDR_RANGE 
 	of the current row*/
-	uint32_t synergy = ADDR_RANGE - bucket_id - 1;
-	bool flag = 0;
-	for (; free_distance =< ADDR_RANGE - 1; ++free_distance)
+	uint32_t synergy = ADDR_RANGE - start_bucket_id - 1;
+	bool flag1 = 0 , flag2 = 0 , flag3 =1;
+	uint32_t mask = 1;
+	uint32_t free_distance = start_bucket_id;//start_bucket_id will be subtracted later on
+	for (;free_distance =< ADDR_RANGE - 1 + start_bucket_id;++free_distance,mask <<= 1)
 	{
-		if (segments_arr[segment][((flag)?(free_distance - synergy + 1):free_distance)]->key == NULL)
+		if (!(mask & start_hop_info) && (segments_arr[segment][((flag)?(free_distance - synergy + 1):free_distance)].key == NULL)){
+			free_bucket = &[segment][((flag)?(free_distance - synergy + 1):free_distance)];
+			flag3 = 0;
 			break;
+		}
 		if (!flag && free_distance => synergy)
 		{
 			flag = 1;
 			segment++;
 		}
 	}
-	if (flag == 1)
-		segment -- ;
-	if (free_distance =< (HOP_SIZE - 1))
-	{
-		
+	/*if (flag == 1)
+		segment --;*/
+	free_distance -= start_bucket_id;
+	if (free_distance < HOP_SIZE){
+		free_bucket->key = *key;
+		free_bucket->data = *data;
+		mask = (1 << free_distance);
+		(start_bucket_id->_hop_info) &= mask ; 
+	}
+	else{
+		do{
+			find_near_free_distance(&free_distance,&free_bucket);
+		}while(free_distance => HOP_SIZE);
+		free_bucket->key = *key;
+		free_bucket->data = *data;
+		mask = (1 << free_distance);
+		(start_bucket_id->_hop_info) &= mask ;
 	}
 }
